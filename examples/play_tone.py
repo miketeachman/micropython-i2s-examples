f545072657590e8328908afc6c8f51f714a6d62a
Mike Teachman <mike.teachman@gmail.com>
2022-02-20 21:24:49 -0800

    refactor tone example

--- examples/play_tone.py
+++ examples/play_tone.py
@@ -17,6 +17,25 @@ import struct
 from machine import I2S
 from machine import Pin
 
+def make_tone(rate, bits, frequency):
+    # create a buffer containing the pure tone samples
+    samples_per_cycle = rate // frequency
+    sample_size_in_bytes = bits // 8
+    samples = bytearray(samples_per_cycle * sample_size_in_bytes)
+    volume_reduction_factor = 32
+    range = pow(2, bits) // 2 // volume_reduction_factor
+    
+    if bits == 16:
+        format = "<h"
+    else:  # assume 32 bits
+        format = "<l"
+    
+    for i in range(samples_per_cycle):
+        sample = range + int((range - 1) * math.sin(2 * math.pi * i / samples_per_cycle))
+        struct.pack_into(format, samples, i * sample_size_in_bytes, sample)
+        
+    return samples
+
 if os.uname().machine.count("PYBv1"):
 
     # ======= I2S CONFIGURATION =======
@@ -92,21 +111,7 @@ audio_out = I2S(
     ibuf=BUFFER_LENGTH_IN_BYTES,
 )
 
-# create a buffer containing the pure tone samples
-samples_per_cycle = SAMPLE_RATE_IN_HZ // TONE_FREQUENCY_IN_HZ
-sample_size_in_bytes = SAMPLE_SIZE_IN_BITS // 8
-samples = bytearray(samples_per_cycle * sample_size_in_bytes)
-volume_reduction_factor = 32
-range = pow(2, SAMPLE_SIZE_IN_BITS) // 2 // volume_reduction_factor
-
-if SAMPLE_SIZE_IN_BITS == 16:
-    format = "<h"
-else:  # assume 32 bits
-    format = "<l"
-
-for i in range(samples_per_cycle):
-    sample = range + int((range - 1) * math.sin(2 * math.pi * i / samples_per_cycle))
-    struct.pack_into(format, samples, i * sample_size_in_bytes, sample)
+samples = make_tone(SAMPLE_RATE_IN_HZ, SAMPLE_SIZE_IN_BITS, TONE_FREQUENCY_IN_HZ)
 
 # continuously write tone sample buffer to an I2S DAC
 print("==========  START PLAYBACK ==========")
